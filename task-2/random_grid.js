48699ebd0616600f23b095f412630ae9607bc0d0
Silviu Catrina <silviu.catrina@gmail.com>
2020-03-29 22:36:40 +0300

    Complete task

--- task-2/random_grid.js
+++ task-2/random_grid.js
@@ -1,5 +1,27 @@
+
 function main() {
     // event handlers and other code here
+
+    gridContainer = document.getElementsByClassName('container')[0];
+
+
+    for (let i = 0; i < 100; i++) {
+        cellForGrid = document.createElement("div");
+        cellForGrid.setAttribute("class", "cell");
+        gridContainer.appendChild(cellForGrid);
+        if (i % 10 == 0 && i != 0) {
+            rowSeparator = document.createElement("br");
+            gridContainer.appendChild(rowSeparator);
+        }
+    }
+
+    allCells = document.getElementsByClassName('cell');
+
+    for (let i = 0; i < 100; i++) {
+      setTimeout(function changeClass() {
+          allCells[i].classList.add('active');
+        }, i * 500);
+    }
 }
 
 main();
